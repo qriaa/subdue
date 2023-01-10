9df6bdf7d8495802328dd11f136e53eea34ac5b6
Maciej Madry <maciek.madry@o2.pl>
2023-01-10 17:04:43 +0100

    E

--- app/src/main/java/com/studx/subdue/PaymentReminder.kt
+++ app/src/main/java/com/studx/subdue/PaymentReminder.kt
@@ -3,13 +3,19 @@ package com.studx.subdue
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.studx.subdue.logic.SubLogic
+import java.time.LocalDateTime
 
 
 class PaymentReminder(val ctx: Context, val params: WorkerParameters) : Worker(ctx, params) {
     val title = "Tytul powiadomienia"
     val message = "Fajna tresc"
     override fun doWork(): Result {
-        // TODO("Dodać czytanie z JSONA")
+        SubLogic.loadSubs(ctx);
+        val subscriptions = SubLogic.getSubList()
+        subscriptions.sortBy { subscription -> subscription.dateAnchor}
+        val latestSub = subscriptions[0]
+        if(latestSub.dateAnchor < LocalDateTime.now().plusDays())
         PaymentNotification(ctx).createNotification(title, message)
         System.out.println("poszla notyfikacja")
         return Result.success()
