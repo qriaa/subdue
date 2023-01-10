e37ddb11924712cadce3b18c3e98695fda2b1cc3
qriaa <49167319+qriaa@users.noreply.github.com>
2023-01-10 17:00:41 +0100

    add isNearPayment in sublogic and work on main menu alert

--- app/src/main/java/com/studx/subdue/SubscriptionBox.kt
+++ app/src/main/java/com/studx/subdue/SubscriptionBox.kt
@@ -8,18 +8,26 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material.icons.sharp.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.studx.subdue.logic.SettingsManager
+import com.studx.subdue.logic.SubLogic
 import com.studx.subdue.logic.Subscription
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
@@ -27,6 +35,12 @@ import java.time.temporal.ChronoUnit
 
 @Composable
 fun SubscriptionBox(rowHeight: Int, rowColor: Color, subscription: Subscription){
+    val isNearPayment: Boolean = remember {
+        SubLogic.isNearPayment(
+            subscription,
+            SettingsManager.settings.daysBeforePaymentAlert
+        )
+    }
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
@@ -62,7 +76,7 @@ fun SubscriptionBox(rowHeight: Int, rowColor: Color, subscription: Subscription)
         Box(
             contentAlignment = Alignment.CenterStart,
             modifier = Modifier
-                .fillMaxWidth(fraction = 0.5f)
+                .fillMaxWidth(fraction = 0.4f)
                 .fillMaxHeight()
         )
         {
@@ -73,43 +87,57 @@ fun SubscriptionBox(rowHeight: Int, rowColor: Color, subscription: Subscription)
             )
         }
 
-        Spacer(modifier = Modifier.fillMaxWidth(0.04f))
-
-        Column(modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-        ){
-            Spacer(modifier = Modifier.height(18.dp))
-            Text(
-                text = subscription.cost.toFloat().toString() + " " + subscription.currency, //#TODO bigdecimal do zaokraglenia
-                fontSize = 16.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Text(
-                text= "/ ${subscription.timeInterval}",
-                fontSize = 16.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Box(
-                contentAlignment = Alignment.BottomCenter,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-            ){
-                Text(
-                    text= "Due " + subscription.dateAnchor.toString(),
-                    fontSize = 15.sp,
-                    color= Color.Gray,
-                    textAlign = TextAlign.Center,
+        Box(
+            contentAlignment = Alignment.CenterStart
+        ) {
+            if (isNearPayment){
+                Icon(
+                    painter = painterResource(id = R.drawable.alert),
+                    contentDescription = "warning icon",
                     modifier = Modifier
-                        .padding(10.dp)
-                        .fillMaxWidth()
+                        .fillMaxWidth(0.2f)
+                        .fillMaxHeight()
                 )
             }
 
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+            ){
+                Spacer(modifier = Modifier.height(18.dp))
+                Text(
+                    text = subscription.cost.toFloat().toString() + " " + subscription.currency, //#TODO bigdecimal do zaokraglenia
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text= "/ ${subscription.timeInterval}",
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Box(
+                    contentAlignment = Alignment.BottomCenter,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                ){
+                    Text(
+                        text= "Due " + subscription.dateAnchor.toString(),
+                        fontSize = 15.sp,
+                        color= Color.Gray,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth()
+                    )
+                }
+
+            }
         }
+
     }
 }
 
